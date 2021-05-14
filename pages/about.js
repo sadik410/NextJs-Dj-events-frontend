b150f499395659c77baff3f4966bbc33d25f7431
Sadik Milliti <sadikosad@gmail.com>
2021-05-14 13:01:09 +0200

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>this is an app to find the lastest DJ and outher musical events</p>
+            <p>version: 1.0.0</p>
+        </div>
+    )
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EventPage() {
+    return (
+        <div>
+            <h1>My events</h1>
+        </div>
+    )
+}
